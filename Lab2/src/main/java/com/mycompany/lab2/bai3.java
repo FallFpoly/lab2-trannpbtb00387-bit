abf4a367343a945dbcfa4f3392b5d98fd0c3f071
trannpbtb00387-bit <trannpbtb00387@fpt.edu.vn>
2025-09-24 23:24:25 +0700

    Lab2_bai3

--- Lab2/src/main/java/com/mycompany/lab2/bai3.java
+++ Lab2/src/main/java/com/mycompany/lab2/bai3.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.lab2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ICT
@@ -11,6 +13,14 @@ package com.mycompany.lab2;
 public class bai3 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int sodien,tien;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap so dien: ");
+        sodien = sc.nextInt();
+        if(sodien<50){
+            System.out.print("Gia tien so dien la: "+(sodien*1000));
+        }else {
+            System.out.println("Gia tien so dien la: "+(50*1000+(sodien-50)*1200));
+        }
     }
 }
